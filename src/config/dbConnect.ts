ccf3b349e3e57c3aa098158b0124b20994274cc4
Vo Minh Thong [James][FE] <thong.vo@navercorp.com>
2025-02-07 20:45:56 +0700

    chore: add log

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async (): Promise<void> => {
   try {
     const mongoURI = process.env.MONGO_URL;
+    console.log('🚀 ~ connectDB ~ mongoURI:', mongoURI);
     await mongoose.connect(mongoURI);
   } catch (err) {
     console.log(err);
